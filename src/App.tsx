facbcd3e97c9bb490e144d601c513accf4883748
alekseidarafeichyk <alekseidarafeichyk@gmail.com>
2020-11-25 23:29:08 +0300

    add layout product

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
+import AppBar from '@material-ui/core/AppBar';
 import React from 'react';
 import './App.css';
+import {Button, Toolbar} from '@material-ui/core';
+import {Catalog} from './components/catalog/Catalog';
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <AppBar position="static">
+                <Toolbar>
+                    <Button color="inherit">Cart</Button>
+                </Toolbar>
+            </AppBar>
+            <Catalog/>
+        </div>
+    );
 }
 
 export default App;
